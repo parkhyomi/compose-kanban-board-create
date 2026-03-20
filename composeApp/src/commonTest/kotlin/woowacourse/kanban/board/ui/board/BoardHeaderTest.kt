7115ea012d60a938bcfa0d725c8ace4914953d88
parkhyomi <phyomi3@naver.com>
2026-03-20 17:24:09 +0900

    test(BoardHeaderTest): 칸반 보드 헤더_버튼 테스트 코드

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/board/BoardHeaderTest.kt
@@ -0,0 +1,58 @@
+package woowacourse.kanban.board.ui.board
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class BoardHeaderTest {
+
+    @Test
+    fun `생성하기 버튼을 눌렀을 때 KanbanDialog가 뜬다`() = runComposeUiTest {
+
+        setContent {
+            KanbanBoard()
+        }
+
+        // 칸반 크리에이트 헤더에 있는 icon 라벨로 구분
+        onNodeWithContentDescription("테스크 종료", useUnmergedTree = true)
+            .assertDoesNotExist()
+
+        onNodeWithText("새 테스크 생성").performClick()
+
+        onNodeWithText("새 태스크 생성", useUnmergedTree = true).assertExists()
+        onNodeWithContentDescription("테스크 종료", useUnmergedTree = true).assertExists()
+    }
+
+    @Test
+    fun `다이얼로그 닫기 클릭 시 다이얼로그가 닫힌다`() = runComposeUiTest {
+        setContent {
+            KanbanBoard()
+        }
+
+        onNodeWithText("새 테스크 생성").performClick()
+        onNodeWithContentDescription("테스크 종료", useUnmergedTree = true).performClick()
+
+        onNodeWithContentDescription("테스크 종료", useUnmergedTree = true)
+            .assertDoesNotExist()
+    }
+
+
+    @Test
+    fun `완료율은 Done 나누기 전체 테스크 수 곱하기 100 으로 계산되어 표시된다`() {
+
+    }
+
+    @Test
+    fun `전체 테스크 수가 0이면 완료율은 0으로 표시한다`() {
+
+    }
+
+    @Test
+    fun `완료율에 맞춰 바의 길이가 변한다`() {
+
+    }
+}
