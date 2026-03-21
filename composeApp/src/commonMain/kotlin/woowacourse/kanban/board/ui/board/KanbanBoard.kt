8f52d2cbf474ae2419dcea85a95c252ef55af0fc
parkhyomi <phyomi3@naver.com>
2026-03-21 15:03:32 +0900

    style: divider추가 및 배경색 지정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoard.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoard.kt
@@ -1,12 +1,16 @@
 package woowacourse.kanban.board.ui.board
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import woowacourse.kanban.board.ui.create.KanbanCreateDialog
 import woowacourse.kanban.board.ui.preview.KanbanPreview
@@ -15,14 +19,18 @@ import woowacourse.kanban.board.ui.preview.KanbanPreview
 fun KanbanBoard() {
     var showDialog by remember { mutableStateOf(false) }
 
-    BoardInfoHeader(
-        totalCount = 10,
-        doneCount = 5,
-        onTaskCreate = {
-            showDialog = true
-        },
-        modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
-    )
+    Column() {
+        BoardInfoHeader(
+            totalCount = 10,
+            doneCount = 5,
+            onTaskCreate = {
+                showDialog = true
+            },
+            modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+                .background(Color.White),
+        )
+        HorizontalDivider()
+    }
     if (showDialog) {
         KanbanCreateDialog(
             onDismissRequest = { showDialog = false }
